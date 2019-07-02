5de58801129dca7403d051625138bb191616e019
gugelm <gugelm@gmail.com>
2019-07-01 20:43:33 -0500

    test

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+module.exports = {
+  mongoURI: 'mongodb://root:vibe@localhost:27017',
+  pusherAppId: '814397',
+  pusherKey: '6965035497b7b1ae0181',
+  pusherSecret: 'f959c1b93d03deff1dce',
+  pusherCluster: 'us2',
+  pusherEncrypted: true
+};
